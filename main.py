41cfe0129aba62ef9de7b64d93296c14a2036e80
kichi004 <100912526+kichi004@users.noreply.github.com>
2022-10-29 17:42:14 -0700

    moved slider code

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import array as arr
+import py_compile
 
-class Slider:
-    def __init__(self):
-        self.a = arr.array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9])    # array representing the game board, 9 = blank
-        self.l = 3  # side length of the array
-        self.p = 8  # position of the blank
-        self.path = ""  # string to track previous actions taken
+from slider import Slider
+from queue import Queue
 
+list = [1, 2, 3, 4, 5, 6, 7, 9, 8]
+
+x = Slider()
+x.input(list)
+
+y = Slider()
+y.copy(x)
+
+y.printGrid()
