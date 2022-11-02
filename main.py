5fc0106ff10ab3dd2604f3418f6ce998722ee3be
kichi004 <100912526+kichi004@users.noreply.github.com>
2022-11-01 20:01:49 -0700

    optimization and readability

--- main.py
+++ main.py
@@ -2,31 +2,44 @@ import py_compile
 import time
 
 from slider import Slider
-from search import Search, UniformSearch, ManhattanSearch, MisplacedSearch
+from search import GeneralSearch, UniformSearch, ManhattanSearch, MisplacedSearch
 
 def eightPuzzleSolver():
-    selection = input("\nWelcome to my Eight Puzzle Solver. Type '1' to use a default puzzle, or '2' to create your own: ")
-    if selection == "1":
+    puzzleSelection = input("\nWelcome to my Eight Puzzle Solver. Type '1' to use a default puzzle, or '2' to create your own: ")
+    if puzzleSelection == "1":
+        listIm = [1, 2, 3, 4, 5, 6, 8, 7, 9]
+        list0 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        list2 = [1, 2, 3, 4, 5, 6, 9, 7, 8]
+        list4 = [1, 2, 3, 5, 9, 6, 4, 7, 8]
+        list8 = [1, 3, 6, 5, 9, 2, 4, 7, 8]
+        list12 = [1, 3, 6, 5, 9, 7, 4, 8, 2]
+        list16 = [1, 6, 7, 5, 9, 3, 4, 8, 2]
+        list20 = [7, 1, 2, 4, 8, 5, 6, 3, 9]
         list = [9, 7, 2, 4, 6, 1, 3, 5, 8]
     else:
         list = []
-        print("\nEnter your puzzle, using the highest value integer to represent the blank. Please only enter valid 8-Puzzles. Enter the puzzle demilimiting the number with a space. Type RETURN only when finished.")
+        print("\nEnter your puzzle, using the highest value integer to represent the blank. Please only enter valid 8-Puzzles. Enter the puzzle delimiting the numbers with a space. Type RETURN only when finished.")
         firstRow = input("Enter the first row: ")
-        rowInput(list, firstRow)
+        insertRow(list, firstRow)
         secondRow = input("Enter the second row: ")
-        rowInput(list, secondRow)
+        insertRow(list, secondRow)
         thirdRow = input("Enter the third row: ")
-        rowInput(list, thirdRow)
+        insertRow(list, thirdRow)
+        # add additional rows if you want to solve 16- or 25- puzzles.
+        '''
+        fourthRow = input("Enter the fourth row: ")
+        insertRow(list, fourthRow)
+        '''
     x = Slider(list)
 
-    searcher = Search(x)
-    algorithm = input("\nSelect an algorithm. (1) for Uniform Cost Search, (2) for the Misplaced Tile Heuristic, or (3) for the Manhattan Distance Heuristic: ")
-    if algorithm == "2":
-        searcher.__class__ = MisplacedSearch
-    elif algorithm == "3":
-        searcher.__class__ = ManhattanSearch
-    else:
+    searcher = GeneralSearch(x)
+    algorithmSelection = input("\nSelect an algorithm. (1) for Uniform Cost Search, (2) for the Misplaced Tile Heuristic, or (3) for the Manhattan Distance Heuristic: ")
+    if algorithmSelection == "1":
         searcher.__class__ = UniformSearch
+    elif algorithmSelection == "2":
+        searcher.__class__ = MisplacedSearch
+    else:
+        searcher.__class__ = ManhattanSearch
 
     print("\nSearching...")
     start = time.perf_counter()
@@ -36,12 +49,14 @@ def eightPuzzleSolver():
         searcher.followSolution(y.getPath())
     print("Search took " + str(round(stop-start, 2)) + " seconds to run.\n")
 
-
-def rowInput(list, row):
+def insertRow(list, row):
     input = row
     while (len(input)!=0):
         input = input.strip(" ")
-        list.append(int(input[0]))
-        input = input[1:]
+        if (len(input) > 1):
+            list.append(int(str(input[0]+input[1]))) # included to support 2-digit values
+        else:
+            list.append(int(input[0]))
+        input = input[2:]
 
 eightPuzzleSolver()
--- search.py
+++ search.py
@@ -1,125 +1,112 @@
 from slider import Slider
 import copy
 
-class Search:
+class GeneralSearch:
     def __init__(self, Slider):
-        self.nodes = []
-        self.pastDict = {}
-        self.origin = Slider
-        self.name = ""
-        self.parsed = 0
-        self.maxQueueSize = 0
+        self.queue = [] # queue of states to examine
+        self.pastDict = {} # previously visited states
+        self.initialState = Slider # initial state to trace from later
+        self.numParsed = 0 # number of parsed states
+        self.maxQueueSize = 0 # maximum queue size achieved
 
-    def search(self):
-        self.pastDict[self.origin.getHash()] = self.makeHeuristicValue(self.origin)
-        self.nodes.append(self.origin)
-        numParsed = 0
+    def search(self): # general search algorithm 
+        self.pastDict[self.initialState.getHash()] = True
+        self.queue.append(self.initialState)
+        self.numParsed = 0
         while(True):
-            if len(self.nodes) > self.maxQueueSize:
-                self.maxQueueSize = len(self.nodes)
-            if not self.nodes:
-                print (self.getName() + " checked through " + str(numParsed) + " states and failed to find a solution.\n")
-                return self.origin
-            current = self.nodes.pop(0)
-            numParsed += 1
+            if len(self.queue) > self.maxQueueSize:
+                self.maxQueueSize = len(self.queue)
+            if not self.queue:
+                print (self.getName() + " checked through " + str(self.numParsed) + " states and failed to find a solution.\n")
+                print("Max queue size: " + str(self.maxQueueSize) + "\n")
+                return self.initialState
+            current = self.queue.pop(0)
+            self.numParsed += 1
             if current.isCorrect():
-                print (self.getName() + " checked through " + str(numParsed) + " states and found a solution at depth " + str(current.getDepth()) + ".")
-                self.parsed = numParsed
+                print (self.getName() + " checked through " + str(self.numParsed) + " states and found a solution at depth " + str(current.getDepth()) + ".")
                 return current
-            # print("current is " + current.getPath(), end=", ")
-            self.queue(current)
+            self.expand(current)
 
-    def inPast(self, Slider):
+    def inPast(self, Slider): # returns true if the game state can be found in the past dictionary
         return Slider.getHash() in self.pastDict
 
-    def getIndex(self, value):
-        for i in range(len(self.nodes)):
-            if (self.makeHeuristicValue(self.nodes[i])) > value:
+    def getIndex(self, value): # returns the index of the state's correct position in the queue
+        for i in range(len(self.queue)):
+            if (self.getPriorityValue(self.queue[i])) > value:
                 return int(i)
-        return len(self.nodes)
+        return len(self.queue)
 
-    def findLowest(self):
-        for i in self.nodes:
-            print(str(self.makeHeuristicValue(i)),end=", ")
-
-    def queue(self, Slider):
+    def expand(self, Slider): # attempts all 4 operations on a state and queues them
         up = copy.deepcopy(Slider)
-        if up.moveUp():
-            #print(self.makeHeuristicValue(up), end="!")
-            self.appendToQueue(up)
+        if up.moveUp(): # movement functions return false if the move is not legal 
+            self.queueingFunction(up)
         down = copy.deepcopy(Slider)
         if down.moveDown():
-            #print(self.makeHeuristicValue(down), end="!")
-            self.appendToQueue(down)
+            self.queueingFunction(down)
         left = copy.deepcopy(Slider)
         if left.moveLeft():
-            #print(self.makeHeuristicValue(left), end="!")
-            self.appendToQueue(left)
+            self.queueingFunction(left)
         right = copy.deepcopy(Slider)
         if right.moveRight():
-            #print(self.makeHeuristicValue(right), end="!")
-            self.appendToQueue(right)
+            self.queueingFunction(right)
 
-    def followSolution(self, route):
+    def followSolution(self, route): # follows the path of the solution to write a traceback
         path = route
         print("\nThe initial state of the puzzle is...")
-        holder = copy.deepcopy(self.origin)
-        holder.printGrid()
+        solutionFollower = copy.deepcopy(self.initialState)
+        solutionFollower.print()
         while (len(path) != 0):
             direction = path[0]
-            holder = holder.followPath(direction)
-            print("The best state to expand with a g(n) = " + str(holder.getDepth()) + self.getHeuristicStatement(holder) + " is...")
-            holder.printGrid()
+            solutionFollower = solutionFollower.followPath(direction)
+            print("The best state to expand with a g(n) = " + str(solutionFollower.getDepth()) + self.getHeuristicStatement(solutionFollower) + " is...")
+            solutionFollower.print()
             path = path[1:]
         print("Goal State!\n")
         print("Solution depth was " + str(len(route)) + ", taking the path " + str(route) + ".")
-        print("Number of nodes expanded: " + str(self.parsed))
+        print("Number of nodes expanded: " + str(self.numParsed))
         print("Max queue size: " + str(self.maxQueueSize) + "\n")
 
-class UniformSearch(Search):
-    def getName(self):
+class UniformSearch(GeneralSearch):
+    def getName(self):  # returns name for print statement
         return "Uniform Cost Search"
 
-    def makeHeuristicValue(self, Slider):
+    def getPriorityValue(self, Slider): # normally returns h(n) + g(n), unused for uniform search 
         return True
 
-    def getHeuristicStatement(self, Slider):
+    def getHeuristicStatement(self, Slider): # also unused for uniform search
         return ""
     
-    def appendToQueue(self, Slider):
+    def queueingFunction(self, Slider): # confirms if already checked, then appends new state to the queue
         if not self.inPast(Slider):
-            self.pastDict[Slider.getHash()] = self.makeHeuristicValue(Slider)
-            self.nodes.append(Slider)
+            self.pastDict[Slider.getHash()] = True
+            self.queue.append(Slider)
 
-class MisplacedSearch(Search):
-    def getName(self):
+class MisplacedSearch(GeneralSearch):
+    def getName(self): # return name for print statement
         return "Misplaced Tile Heuristic"
 
-    def makeHeuristicValue(self, Slider):
-        return Slider.getMisplacedTilesHeuristic()
+    def getPriorityValue(self, Slider): # returns h(n) + g(n) named "priority value"
+        return Slider.getMisplacedTilesPriorityValue()
     
-    def getHeuristicStatement(self, Slider):
-        return " and h(n) = " + str(Slider.getMisplacedTiles())
+    def getHeuristicStatement(self, Slider): # returns string statement for traceback
+        return " and h(n) = " + str(Slider.getMisplacedTilesHeuristic())
 
-    def appendToQueue(self, Slider):
+    def queueingFunction(self, Slider): # adds to queue, asking the getIndex function for correct spot
         if not self.inPast(Slider):
-            heuristicValue = self.makeHeuristicValue(Slider)
-            self.pastDict[Slider.getHash()] = heuristicValue
-            self.nodes.insert(self.getIndex(heuristicValue), Slider)
+            self.pastDict[Slider.getHash()] = True
+            self.queue.insert(self.getIndex(self.getPriorityValue(Slider)), Slider)
 
-class ManhattanSearch(Search):
-    def getName(self):
+class ManhattanSearch(GeneralSearch):
+    def getName(self): # returns name for print statement
         return "Manhattan Distance Heuristic"
 
-    def makeHeuristicValue(self, Slider):
-        return Slider.getManhattanHeuristic()
+    def getPriorityValue(self, Slider): # return h(n) + g(n) named "priority value"
+        return Slider.getManhattanDistancePriorityValue()
 
-    def getHeuristicStatement(self, Slider):
-        return " and h(n) = " + str(Slider.getManhattanDistance())
+    def getHeuristicStatement(self, Slider): # returns string statement for traceback
+        return " and h(n) = " + str(Slider.getManhattanDistanceHeuristic())
 
-    def appendToQueue(self, Slider):
+    def queueingFunction(self, Slider): #adds to queue, asking the getIndex function for correct spot
         if not self.inPast(Slider):
-            heuristicValue = self.makeHeuristicValue(Slider)
-            self.pastDict[Slider.getHash()] = heuristicValue
-            # print(self.getIndex(heuristicValue), end="@")
-            self.nodes.insert(self.getIndex(heuristicValue), Slider)
+            self.pastDict[Slider.getHash()] = True
+            self.queue.insert(self.getIndex(self.getPriorityValue(Slider)), Slider)
